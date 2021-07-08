ce80c164a68cad40b5b3b2c6f0a168815e0a6502
Jatin Rathee <jatin.rathee@razorpay.com>
2021-07-08 21:39:21 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./app.styl";
+
+const App = () => {
+    return (
+        <div>
+            <h2>Working fine</h2>
+        </div>
+    );
+};
+
+export default App;
